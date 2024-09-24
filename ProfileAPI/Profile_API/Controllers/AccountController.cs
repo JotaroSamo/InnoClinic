719c0be6214e2dd7f16281cca3f24c5c72496775
HONOR <HONOR@LAPTOP-OUCFRN3I>
2024-09-24 15:42:12 +0300

    controllerAccount

--- ProfileAPI/Profile_API/Controllers/AccountController.cs
+++ ProfileAPI/Profile_API/Controllers/AccountController.cs
@@ -15,6 +15,7 @@ namespace Profile_API.Controllers
         {
             _accountService = accountService;
         }
+
         [HttpPost("Create")]
         public async Task<ActionResult> Create([FromBody] CreateAccountRequest createAccountRequest)
         {
@@ -40,6 +41,18 @@ namespace Profile_API.Controllers
             account = await _accountService.CreateAccountAsync(account);
             return Ok(account);
 
+        }
+        [HttpGet("GetById")]
+        public async Task<ActionResult> GetById(Guid id)
+        {
+            var account = await _accountService.GetAccountByIdAsync(id);
+            return Ok(account);
+        }
+        [HttpGet("GetAll")]
+        public async Task<ActionResult> GetAll()
+        {
+            var accounts = await _accountService.GetAllAccountsAsync();
+            return Ok(accounts);
         }
         [HttpGet("verify-email")]
         public async Task<ActionResult> VerifyEmail(Guid userId)
@@ -82,6 +95,12 @@ namespace Profile_API.Controllers
             }
             account = await _accountService.UpdateAccountAsync(account.Id,account);
             return Ok(account);
+        }
+        [HttpDelete("Delete")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _accountService.DeleteAccountAsync(id);
+            return Ok();
         }
 
     }
