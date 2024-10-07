eb9a542756edfd813de1c07ce6c488313503bce6
HONOR <HONOR@LAPTOP-OUCFRN3I>
2024-10-07 13:15:38 +0300

    add Application

---
+++ AppointmentAPI/Appointment_API.Application/Service/PatientAppointmentService.cs
@@ -0,0 +1,52 @@
+﻿using Appointment_API.DataAccess.Repository;
+using Appointment_API.Domain.Model;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_API.Application.Service
+{
+    public class PatientAppointmentService 
+    {
+        private readonly IPatientRepository _patienRepository;
+
+        public PatientAppointmentService(IPatientRepository patientRepository)
+        {
+            _patienRepository = patientRepository;
+        }
+
+        public async Task<Result<PatientAppointment>> CreatePatient(PatientAppointment patient)
+        {
+            var result = await _patienRepository.Create(patient);
+            return result;
+        }
+
+        public async Task<Result> DeletePatient(Guid id)
+        {
+            var result = await _patienRepository.Delete(id);
+            return result;
+        }
+
+        public async Task<Result<List<PatientAppointment>>> GetAllPatients()
+        {
+            var result = await _patienRepository.GetAll();
+            return result;
+        }
+
+        public async Task<Result<PatientAppointment>> GetPatientById(Guid id)
+        {
+            var result = await _patienRepository.GetById(id);
+            return result;
+        }
+
+        public async Task<Result<PatientAppointment>> UpdatePatient(PatientAppointment patient)
+        {
+            var result = await _patienRepository.Update(patient);
+            return result;
+        }
+    }
+
+}
---
+++ AppointmentAPI/Appointment_API.Application/Service/ServiceAppointmentService.cs
@@ -0,0 +1,52 @@
+﻿using Appointment_API.DataAccess.Repository;
+using Appointment_API.Domain.Model;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_API.Application.Service
+{
+    public class ServiceAppointmentService 
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public ServiceAppointmentService(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+        public async Task<Result<ServiceAppointment>> CreateService(ServiceAppointment service)
+        {
+            var result = await _serviceRepository.Create(service);
+            return result;
+        }
+
+        public async Task<Result> DeleteService(Guid id)
+        {
+            var result = await _serviceRepository.Delete(id);
+            return result;
+        }
+
+        public async Task<Result<List<ServiceAppointment>>> GetAllServices()
+        {
+            var result = await _serviceRepository.GetAll();
+            return result;
+        }
+
+        public async Task<Result<ServiceAppointment>> GetServiceById(Guid id)
+        {
+            var result = await _serviceRepository.GetById(id);
+            return result;
+        }
+
+        public async Task<Result<ServiceAppointment>> UpdateService(ServiceAppointment service)
+        {
+            var result = await _serviceRepository.Update(service);
+            return result;
+        }
+    }
+
+}
